bb7fe46e57828207121f2b5a93bf629858d4a621
Nanda <nandarameshan21@gmail.com>
2026-01-17 18:55:07 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+function changecontent() {
+    let x = document.getElementById("know-more");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+function valid() {
+    let x = document.getElementById("name-input").value;
+    let y = document.getElementById("email-input").value;
+    let z = document.getElementById("message-input").value;
+    if (x == "") {
+        document.getElementById("name-validation").style.display = "block";
+    }
+    if (y == "") {
+        document.getElementById("email-validation").style.display = "block";
+    }
+    if (z == "") {
+        document.getElementById("message-validation").style.display = "block";
+    }
+    if ((x || y || z) == "") {
+        return false;
+    }
+}
